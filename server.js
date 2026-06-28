e24b98651dadcaccdc8e5b8a6de9b4faa2b35af5
amitshabatgoltz-create <amitshabatgoltz@gmail.com>
2026-06-28 12:31:30 +0300

    5 players lobby + names

--- server.js
+++ server.js
@@ -9,136 +9,117 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Game state
-const rooms = {};
+const MAX_PLAYERS = 5;
+const room = {
+  players: {},
+  state: 'waiting',
+  winner: null,
+  raceStart: null
+};
 
-function createRoom(id) {
-  return {
-    id,
-    players: {},
-    state: 'waiting', // waiting | countdown | racing | finished
-    countdownVal: 3,
-    raceStart: null,
-    winner: null
-  };
-}
-
-const LOBBY = 'main';
-rooms[LOBBY] = createRoom(LOBBY);
+// Start positions around the oval
+const startPositions = [
+  {x:90, z:0}, {x:86, z:3}, {x:86, z:-3}, {x:82, z:5}, {x:82, z:-5}
+];
+const startTs = [0.003, 0.008, 0.013, 0.018, 0.023];
 
 io.on('connection', (socket) => {
-  console.log('Player connected:', socket.id);
-  const room = rooms[LOBBY];
-
-  // Assign player number
   const playerCount = Object.keys(room.players).length;
-  if (playerCount >= 2) {
+  if(playerCount >= MAX_PLAYERS){
     socket.emit('room_full');
     return;
   }
 
-  const playerNum = playerCount + 1;
-  room.players[socket.id] = {
-    id: socket.id,
-    num: playerNum,
-    name: 'Player ' + playerNum,
-    ready: false,
-    // Physics state
-    x: playerNum === 1 ? 90 : 87,
-    z: 0,
-    angle: Math.PI / 2,
-    speed: 0,
-    lap: 1,
-    t: playerNum === 1 ? 0.003 : 0.010,
-    finished: false,
-    bestLap: null,
-    lapStart: 0
-  };
+  socket.on('join', (data) => {
+    const num = Object.keys(room.players).length + 1;
+    const sp = startPositions[num-1];
+    room.players[socket.id] = {
+      id: socket.id,
+      num,
+      name: data.name || 'Player '+num,
+      ready: false,
+      x: sp.x, z: sp.z,
+      angle: Math.PI/2,
+      speed: 0,
+      lap: 1,
+      t: startTs[num-1],
+      finished: false,
+      bestLap: null,
+      wins: data.wins || 0
+    };
 
-  socket.join(LOBBY);
-  socket.emit('joined', {
-    yourId: socket.id,
-    yourNum: playerNum,
-    players: room.players,
-    roomState: room.state
+    socket.join('main');
+    socket.emit('joined', {
+      yourId: socket.id,
+      yourNum: num,
+      players: room.players,
+      roomState: room.state
+    });
+    socket.to('main').emit('player_joined', room.players[socket.id]);
+    console.log(`${data.name} joined as P${num}. Total: ${num}`);
   });
-  socket.to(LOBBY).emit('player_joined', { id: socket.id, num: playerNum });
 
-  console.log(`Player ${playerNum} joined. Total: ${Object.keys(room.players).length}`);
-
-  // Start countdown when 2 players ready
   socket.on('player_ready', () => {
-    if (!room.players[socket.id]) return;
+    if(!room.players[socket.id]) return;
     room.players[socket.id].ready = true;
-    io.to(LOBBY).emit('player_ready', { id: socket.id });
+    io.to('main').emit('player_ready', { id: socket.id });
 
-    const allReady = Object.values(room.players).length === 2 &&
-                     Object.values(room.players).every(p => p.ready);
-    if (allReady && room.state === 'waiting') {
+    const players = Object.values(room.players);
+    const allReady = players.length >= 2 && players.every(p => p.ready);
+    if(allReady && room.state === 'waiting'){
       room.state = 'countdown';
-      startCountdown(room);
+      startCountdown();
     }
   });
 
-  // Receive position update from client
   socket.on('update', (data) => {
-    if (!room.players[socket.id]) return;
+    if(!room.players[socket.id]) return;
     Object.assign(room.players[socket.id], data);
-    // Broadcast to OTHER player
-    socket.to(LOBBY).emit('opponent_update', {
-      id: socket.id,
-      ...data
-    });
-  });
-
-  socket.on('lap_complete', (data) => {
-    io.to(LOBBY).emit('lap_complete', { id: socket.id, ...data });
+    socket.to('main').emit('player_update', { id: socket.id, ...data });
   });
 
   socket.on('finished', (data) => {
-    if (!room.players[socket.id]) return;
+    if(!room.players[socket.id]) return;
     room.players[socket.id].finished = true;
-    if (!room.winner) {
+    if(!room.winner){
       room.winner = socket.id;
-      io.to(LOBBY).emit('race_finished', {
+      io.to('main').emit('race_finished', {
         winnerId: socket.id,
-        winnerNum: room.players[socket.id].num,
+        winnerName: room.players[socket.id].name,
         time: data.time
       });
     }
   });
 
   socket.on('disconnect', () => {
-    console.log('Player disconnected:', socket.id);
-    if (room.players[socket.id]) {
-      const num = room.players[socket.id].num;
-      delete room.players[socket.id];
-      io.to(LOBBY).emit('player_left', { id: socket.id, num });
-      // Reset room
+    if(!room.players[socket.id]) return;
+    const name = room.players[socket.id].name;
+    delete room.players[socket.id];
+    io.to('main').emit('player_left', { id: socket.id, name });
+    // Reset if everyone leaves
+    if(Object.keys(room.players).length === 0){
       room.state = 'waiting';
       room.winner = null;
-      Object.values(room.players).forEach(p => p.ready = false);
     }
+    console.log(`${name} disconnected`);
   });
 });
 
-function startCountdown(room) {
+function startCountdown(){
   let val = 3;
-  io.to(room.id).emit('countdown', { val });
+  io.to('main').emit('countdown', { val });
   const interval = setInterval(() => {
     val--;
-    if (val <= 0) {
+    if(val <= 0){
       clearInterval(interval);
       room.state = 'racing';
       room.raceStart = Date.now();
-      io.to(room.id).emit('race_start', { time: room.raceStart });
+      io.to('main').emit('race_start', { time: room.raceStart });
     } else {
-      io.to(room.id).emit('countdown', { val });
+      io.to('main').emit('countdown', { val });
     }
   }, 1000);
 }
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`🏎️  Racing server running on http://localhost:${PORT}`);
-});
+server.listen(PORT, () => console.log(`🏎️  Racing server on http://localhost:${PORT}`));
